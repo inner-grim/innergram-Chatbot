79923bf44e709bff6a46013a04a5e308321c011e
woopi1030 <lozenkrantz@gmail.com>
2024-11-14 02:05:10 +0900

    [CHG] Parameter Store 값 세팅 로직 수정

--- chatbot_backend/chatbot_api/views.py
+++ chatbot_backend/chatbot_api/views.py
@@ -8,13 +8,11 @@ from rest_framework.decorators import api_view
 from rest_framework import status
 import boto3
 
+# Parameter Store를 통해 API KEY 값 세팅
 def fetch_api_key_from_parameter_store(parameter_name):
     ssm = boto3.client('ssm')
     response = ssm.get_parameter(Name=parameter_name, WithDecryption=True)
     return response['Parameter']['Value']
-
-api_key = fetch_api_key_from_parameter_store('/parameter/chatbot/api.key')
-os.environ["OPENAI_API_KEY"] = api_key # 보안을 위해 API KEY 여기서 뿌려줌
 
 @api_view(['POST'])
 def chatbot_response(request):
@@ -22,6 +20,9 @@ def chatbot_response(request):
     사용자 입력을 ChatGPT 모델로 전송하고, 응답을 반환하는 API
     '''
     try:
+        api_key = fetch_api_key_from_parameter_store('/parameter/chatbot/api.key')
+        os.environ["OPENAI_API_KEY"] = api_key # 보안을 위해 API KEY 여기서 뿌려줌
+
         user_input = request.data.get('user_input')
 
         if not user_input:
